cf79cdd87a96bac5c85a441ef1ef894282ccade1
Ashmeet Singh Chhabra <ashmeet807@gmail.com>
2025-12-28 17:50:59 +0530

    some tests

--- tests.py
+++ tests.py
@@ -6,13 +6,30 @@ import json
 import task_cli as task
 
 # core features test
-
 class TestTaskCLI(unittest.TestCase):
     def test_add_task(self):
         task.add_task("Buy milk")
         tasks = task.load_tasks()
 
-        self.assertEqual(len(tasks), 1)
+        self.assertEqual(len(tasks), 1);
+        self.assertEqual(tasks[0]['description'], 'Buy milk');
+        self.assertEqual(tasks[0]['status'], 'todo');
+        task.delete_task(1);
+
+    def test_update_task(self):
+        task.add_task('Buy tea')
+        task.update_task(1, "We hate tea. Buy coffee instead")
+        
+        tasks = task.load_tasks()
+        self.assertEqual(tasks[0]['description'], "We hate tea. Buy coffee instead")
+        task.delete_task(1)
+
+    def test_delete_task(self):
+        task.add_task('Cauliflower')
+        task.delete_task(1)
+        tasks = task.load_tasks()
+
+        self.assertEqual(len(tasks), 0)
 
 if __name__ == '__main__':
     unittest.main()
